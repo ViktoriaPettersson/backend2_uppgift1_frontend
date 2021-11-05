cb7671ee22f3d6e0bf14520867a43602b54dec2d
pette <pettersson.viktoria@hotmail.com>
2021-11-05 21:29:03 +0100

    commit

--- src/components/Product/ProductCard.js
+++ src/components/Product/ProductCard.js
@@ -1,38 +1,41 @@
 import React from "react";
 
-const ProductCard = () => {
+// Tar emot product
+const ProductCard = ({ product }) => {
   return (
     <div>
-      {/* card start */}
-      <div className="product_card">
-        <div className="mb-2">Products - Phones</div>
-        <div className="card card-ecommerce">
-          <div className="view zoom product_image">
-            <img
-              src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Products/13.jpg"
-              className="img-fluid"
-              alt="sample"
-            />
-          </div>
+      {/* <div className="row"> */}
+      {/* grid column start */}
+      <div className="col mb-4 d-flex align-items-stretch">
+        {/* card start */}
+        <div className="product_card">
+          <div className="mb-2">Products - {product.subCategoryId}</div>
+          <div className="card card-ecommerce">
+            <div className="view zoom product_image">
+              <img src={product.imageUrl} className="img-fluid" alt="sample" />
+            </div>
 
-          <div className="card-body">
-            <h5 className="card-title mb-1">
-              <strong>
-                <div className="dark-grey-text">Asus CT-567</div>
-              </strong>
-            </h5>
-
-            <div className="white card-footer pb-0">
-              <div className="row mb-0">
-                <span className="float-left">
-                  <strong>1439$</strong>
-                </span>
+            <div className="card-body">
+              <h5 className="card-title mb-1">
+                <strong>
+                  <div className="dark-grey-text">{product.productName}</div>
+                </strong>
+              </h5>
+              <p className="py-2">{product.shortDescription}</p>
+              <div className="white card-footer pb-0">
+                <div className="row mb-0">
+                  <span className="float-left">
+                    <strong>{product.price}$</strong>
+                  </span>
+                </div>
               </div>
             </div>
           </div>
         </div>
+        {/* card end */}
       </div>
-      {/* card end */}
+      {/* grid column end */}
+      {/* </div> */}
     </div>
   );
 };
--- src/views/Products.js
+++ src/views/Products.js
@@ -1,7 +1,24 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import ProductCard from "../components/Product/ProductCard";
 
 const Products = () => {
+  const [products, setProducts] = useState([]);
+  // Funktion som hämtar produkter från databasen via WepApi
+  const getProducts = async () => {
+    const res = await axios.get("https://localhost:44323/api/Products");
+    // hämtar upp produkterna sparar i products
+    const _products = await res.data;
+    // skickar in i setProducts
+    setProducts(_products);
+  };
+  // Körs direkt när komponenten laddas
+  useEffect(() => {
+    //kör funktionen
+    getProducts();
+  });
+
   return (
     <div>
       <div className="container">
@@ -10,47 +27,12 @@ const Products = () => {
         </h1>
 
         {/* row start */}
-        <div className="row">
-          {/* grid column start */}
-          <div className="col-lg-4 col-md-12 mb-4 d-flex align-items-stretch">
-            {/* card start */}
-            <div className="product_card">
-              <Link to="/details">
-                <div className="mb-2 text-dark">Products - Phones</div>
-                <div className="card card-ecommerce">
-                  <div className="view zoom product_image">
-                    <img
-                      src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Products/13.jpg"
-                      className="img-fluid"
-                      alt="sample"
-                    />
-                  </div>
-
-                  <div className="card-body">
-                    <h5 className="card-title mb-1">
-                      <strong>
-                        <div className="dark-grey-text">Asus CT-567</div>
-                      </strong>
-                    </h5>
-                    <div className="text-dark py-2">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      Vivamus sed tincidunt urna. Morbi tortor mauris, porta nec
-                      mi
-                    </div>
-                    <div className="white card-footer pb-0">
-                      <div className="row mb-0">
-                        <span className="float-left">
-                          <strong className="text-dark">1439$</strong>
-                        </span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </Link>
-            </div>
-            {/* card end */}
-          </div>
-          {/* grid column end */}
+        <div className="row row-cols-1 row-cols-md-3">
+          {/* kör en map på product */}
+          {/* Rendera ut productCard och skicka med id som key och product som en product */}
+          {products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
         </div>
         {/* row end */}
       </div>
